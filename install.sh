7cd4c2bae40b34c10f7c663c1cc8628d04333079
sunowsir <sunowsir@qq.com>
2018-07-19 10:55:23 +0800

    version 2.0

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+echo 第一个参数：用户密码
+echo 第二个参数：有线网卡名称
+echo 请检查您的参数是否正确。
+
 nowpath=`pwd`
 
 cd ~/
 
 chmod +x *
 
-echo '############ruijie########' >> ~/.bashrc
+echo "
 
-echo "export \$PD=\"$1\" "  >> ~/.bashrc
-echo "export \$CNC=\"$2\" " >> ~/.bashrc
-echo "alias rj='fun(){ $nowpath/rj.sh \$1; }; fun' " >> ~.bashrc
+    ############ruijie######## 
 
-echo '############ruijie########' >> ~/.bashrc
+    export \$PD=\"$1\" 
+    export \$CNC=\"$2\" 
+    alias rj='fun(){ $nowpath/rj.sh \$1; }; fun'
+
+    ############ruijie######## 
+
+" >> ~/.bashrc
 
 sources ~/.bashrc
